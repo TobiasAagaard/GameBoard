faa236f662c90b46beed56224226daf665bf2ff7
Tobias Aagaard <Tobias909@live.dk>
2022-10-24 10:55:01 +0200

    Images til spillet

---
+++ assets/js/modal.js
@@ -0,0 +1,72 @@
+export const GoalModal = () => {
+  [
+    {
+      goal: "1",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "2",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "3",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "4",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "5",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "6",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "7",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "8",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "9",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "10",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "11",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "12",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "13",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "14",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "15",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "16",
+      title: "afskaf fattigdom",
+    },
+    {
+      goal: "17",
+      title: "afskaf fattigdom",
+    },
+  ];
+};
