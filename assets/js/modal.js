56c721732c3924af705a70470fe9e22c61a8c4b4
Tobias Aagaard <Tobias909@live.dk>
2022-10-24 10:58:57 +0200

    modal opsætningen

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,70 +3,87 @@ export const GoalModal = () => {
     {
       goal: "1",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-01.png",
     },
     {
       goal: "2",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-02.png",
     },
     {
       goal: "3",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-03.png",
     },
     {
       goal: "4",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-04.png",
     },
     {
       goal: "5",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-05.png",
     },
     {
       goal: "6",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-06.png",
     },
     {
       goal: "7",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-07.png",
     },
     {
       goal: "8",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-08.png",
     },
     {
       goal: "9",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-09.png",
     },
     {
       goal: "10",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-10.png",
     },
     {
       goal: "11",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-11.png",
     },
     {
       goal: "12",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-12.png",
     },
     {
       goal: "13",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-13.png",
     },
     {
       goal: "14",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-14.png",
     },
     {
       goal: "15",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-15.png",
     },
     {
       goal: "16",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-16.png",
     },
     {
       goal: "17",
       title: "afskaf fattigdom",
+      image: "../images/Verdensmaal-ikon-17.png",
     },
   ];
 };
