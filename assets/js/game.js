25ee249044acb9e21197f306ff9997e9dc5272c2
Tobias Aagaard <Tobias909@live.dk>
2022-10-27 10:27:40 +0200

    Add
    -randomize
    -lavet elementer
    - appendet ellementer
    -grid

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,39 @@
 import { GoalModal } from "./modal.js";
-
-const gameborad = document.getElementById("gameBorad");
+const gameBorad = document.getElementById("gameBorad");
 const numCards = 10;
 let cardList = GoalModal();
-cardList.sort(() => Math.random() - 0.5);
+
 cardList = cardList.slice(0, numCards);
 cardList = cardList.concat(cardList);
-cardList.sort(() => Math.random() - 0.5);
 
-for (let card of cardList) {
-  let div = document.createElement("div");
-  div.innerHTML = card.goal;
-  gameborad.append(div);
-}
+const randommize = () => {
+  const cardData = cardList;
+  cardData.sort(() => Math.random() - 0.5);
+  cardData.sort(() => Math.random() - 0.5);
+  return cardData;
+};
+
+// Genrate card
+
+const cardGenerate = () => {
+  const cardData = randommize();
+  console.log(cardData);
+  // Html
+  cardData.forEach((item) => {
+    const card = document.createElement("div");
+    const face = document.createElement("img");
+    const back = document.createElement("div");
+
+    card.classList.add("card");
+    face.classList.add("face");
+    back.classList.add("back");
+    // Giver card info med billeder
+    face.src = item.imgSrc;
+    // lægger cards sammen med gameBorad
+    gameBorad.appendChild(card);
+    card.appendChild(face);
+    card.appendChild(back);
+  });
+};
+
+cardGenerate();
